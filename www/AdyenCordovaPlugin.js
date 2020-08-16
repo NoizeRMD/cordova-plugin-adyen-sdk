4aad12c7ea768ba74cf933223f4a815a223b10ee
Moritz Munte <geschan@users.noreply.github.com>
2020-08-16 19:16:17 +0200

    chore: add basic files, dependencies and configs

--- www/AdyenCordovaPlugin.js
+++ www/AdyenCordovaPlugin.js
@@ -1,5 +1,11 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'AdyenCordovaPlugin', 'coolMethod', [arg0]);
+var AdyenCordovaPlugin = function () { };
+// This just makes it easier for us to export all of the functions at once.
+// All of your plugin functions go below this.
+// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
+
+AdyenCordovaPlugin.coolMethod = function (arg0, onSuccess, onError) {
+    exec(onSuccess, onError, "AdyenCordovaPlugin", "coolMethod", [arg0]);
 };
+module.exports = AdyenCordovaPlugin;
